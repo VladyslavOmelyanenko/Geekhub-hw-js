0c576eeb0b945306e91f7649e15a8d59fca8a8fb
Vladyslav Omelyanenko <vladyslavomelyanenko@MacBook-Pro-Vladyslav.local>
2018-12-09 22:29:45 +0200

    added mazes solutions

---
+++ homework2/index.js
@@ -0,0 +1,132 @@
+// level 1
+while(isFree("north")){
+    north();
+}
+
+//level 2
+while(isFree("east")){
+    east();
+}
+
+//level 3, 4
+while(isFree("south") || isFree("east")){
+    south();
+    east();
+}
+
+//level 5
+while(isFree("west")){
+    west();
+}
+
+while(isFree("south")){
+    south();
+}
+
+while(isFree("west")){
+    west();
+}
+
+while(isFree("north")){
+    north();
+}
+
+while(isFree("west")){
+    west();
+}
+
+while(isFree("south")){
+    south();
+}
+
+while(isFree("east")){
+    east();
+}
+
+//level 6
+while(isFree("south")){
+    south();
+}
+
+while(isFree("east")){
+    east();
+}
+
+while(isFree("north")){
+    north();
+}
+
+while(isFree("east")){
+    east();
+}
+
+while(isFree("north")){
+    north();
+}
+
+while(isFree("east")){
+    east();
+}
+
+while(isFree("south")){
+    south();
+}
+
+north()
+
+while(isFree("west")){
+    west();
+}
+
+while(isFree("south")){
+    south();
+}
+
+while(isFree("east")){
+    east();
+}
+
+//level 7
+while(isFree("south")){
+    south();
+}
+
+while(isFree("east")){
+    east();
+}
+
+while(isFree("north")){
+    north();
+}
+
+while(isFree("east")){
+    east();
+}
+
+while(isFree("south")){
+    south();
+}
+
+while(isFree("east")){
+    east();
+}
+
+while(isFree("south")){
+    south();
+}
+
+west()
+
+while(isFree("south")){
+    south();
+}
+
+while(isFree("east")){
+    east();
+}
+
+while(isFree("south")){
+    south();
+}
+
+east();
